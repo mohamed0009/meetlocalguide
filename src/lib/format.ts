29c426f823e337e8ce3c421d67d043ddcdd77f8d
mohamed0009 <mohamed999idrissi@gmail.com>
2026-04-06 20:40:38 +0100

    Remove legacy root frontend files after merge

--- src/lib/format.ts
+++
@@ -1,7 +0,0 @@
-export function formatUsd(value: number): string {
-  return new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    maximumFractionDigits: 0,
-  }).format(value);
-}
